45b20f74af2705619d5b4063f475316f5aba2b2a
olalium <ola.lium@gmail.com>
2020-09-27 22:01:01 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.6
+
+WORKDIR /usr/src/app
+
+# need this for dlib installation later
+RUN pip install cmake
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD [ "python", "./server.py" ]
---
+++ server.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask, flash, request, redirect, url_for
+from flask_cors import CORS, cross_origin
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 # Max size of content is 2MB (1MB per image)
+
+UPLOAD_FOLDER = '/images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def filename_is_valid(filename):
+    if filename == '':
+        return False
+    if not allowed_file(filename):
+        return False
+    return True
+
+@app.route('/api/predict', methods = ['POST'] )
+@cross_origin(allow_headers=['Content-Type'])
+def predict_face():
+    if 'image1' not in request.files:
+        app.logger.info("image1 not ok")
+    if 'image2' not in request.files:
+        app.logger.info("image2 not ok")
+    image1 = request.files['image1']
+    image2 = request.files['image2']
+    if not filename_is_valid(image1.filename) or not filename_is_valid(image2.filename):
+        app.logger.info("one filename is invalid")
+    filename_image1 = secure_filename(image1.filename)
+    filename_image2 = secure_filename(image2.filename)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
